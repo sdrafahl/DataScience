e50d40f8daa7cd6240ad5f7a9a1d3ff135a8038c
Shane P Drafahl <sdrafahl@iastate.edu>
2017-08-14 15:08:04 -0500

    Added displayBarGraph method

--- src/VisualizingData.py
+++ src/VisualizingData.py
@@ -7,8 +7,16 @@ class VisualizingData:
         self.marker = marker
         self.linestyle = linestyle
 
-    def displayPlotGraph(self, xCoords, yCoords, title, ylable):
+    def displayPlotGraph(self, xCoords, yCoords, title, ylabel):
         plt.plot(xCoords, yCoords, color = self.color, marker = self.marker, linestyle = self.linestyle)
         plt.title(title)
-        plt.ylabel(ylable)
+        plt.ylabel(ylabel)
+        plt.show()
+
+    def displayBarGraph(self, xCoords, yCoords, title, ylabel):
+        xs = [i + 0.1 for i, _ in enumerate(xCoords)]
+        plt.bar(xs, yCoords)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.xticks([i + 0.5 for i, _ in enumerate(xCoords)], xCoords)
         plt.show()
