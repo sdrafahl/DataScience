b7de078eb1377313c248a6c0e6c9a456977b0cd0
Shane <drafahls@gmail.com>
2018-03-18 14:40:56 -0500

    Wrote code to enable finding the standard deviation

--- src/Statistics.py
+++ src/Statistics.py
@@ -1,10 +1,23 @@
+import math
+
 class Statistics:
 
     def __init__(self):
         self.dataList = []
+
+    def __init__(self, data):
+        self.dataList = data
 
     def addData(self, value):
         self.dataList.append(value)
 
     def numberOfValues(self):
         return len(self.dataList)
+
+    def standardDeviation(self):
+        n = self.numberOfValues()
+        sampleMean = sum(self.dataList)/n
+        sumValue = 0
+        for x in self.dataList:
+            sumValue += (x - sampleMean) ** 2
+        return math.sqrt((sumValue / n))
--- src/main.py
+++ src/main.py
@@ -8,47 +8,55 @@ from __init__ import *
 #visualizer.displayDistributationGraph(testValues ,10 , "years", "GDP", "billions")
 print "Vector Demo"
 
-vector = Vector()
-vector.addElement(5)
-vector.addElement(9)
-vector.addElement(8)
-vector.addElement(7)
+#vector = Vector()
+#vector.addElement(5)
+#vector.addElement(9)
+#vector.addElement(8)
+#vector.addElement(7)
 
 print "Vector 1"
-vector.printVector()
+#vector.printVector()
 
-vector2 = Vector()
-vector2.addElement(10)
-vector2.addElement(9)
-vector2.addElement(82)
-vector2.addElement(0)
+#vector2 = Vector()
+#vector2.addElement(10)
+#vector2.addElement(9)
+#vector2.addElement(82)
+#vector2.addElement(0)
 
 print "Vector 2"
-vector2.printVector()
+#vector2.printVector()
 
 print "Vector 3 = Vector 2 + Vector 1"
-vector3 = vector.addVector(vector2)
+#vector3 = vector.addVector(vector2)
 
-vector3.printVector()
+#vector3.printVector()
 
 print "Vector Sum of Vector 1, Vector 2, Vector 3"
 
-vectors = [vector, vector2, vector3]
+#vectors = [vector, vector2, vector3]
 
-vector4 = vectorSum(vectors)
-vector4.printVector()
+#vector4 = vectorSum(vectors)
+#vector4.printVector()
 
 print "Scalar Multiply Vector 4"
 
-vector4.scalarMultiply(10)
-vector4.printVector()
+#vector4.scalarMultiply(10)
+#vector4.printVector()
 
 print "Vector Mean"
 
-vector5 = vectorMean(vectors)
+#vector5 = vectorMean(vectors)
 
-vector5.printVector()
+#vector5.printVector()
 
 print "Dot Product"
 
-print dotProduct(vector, vector2)
+#print dotProduct(vector, vector2)
+
+print "Standard Deviation Test"
+
+stats = Statistics([4.52, 4.66, 4.66, 4.66, 4.66, 4.67, 4.67, 4.67, 4.67, 4.67,
+ 4.67, 4.67, 4.68, 4.68, 4.68, 4.68, 4.68, 4.68, 4.69, 4.69, 4.69, 4.69, 4.69, 4.69,
+ 4.69, 4.69, 4.69, 4.69, 4.69, 4.69, 4.69, 4.69, 4.70, 4.70, 4.70, 4.70, 4.70, 4.70,
+ 4.70, 4.70, 4.70, 4.71, 4.71, 4.71, 4.71, 4.72, 4.72, 4.72, 4.71])
+print stats.standardDeviation()
